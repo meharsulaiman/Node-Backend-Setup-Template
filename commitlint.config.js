1adbb1a8ba3bd2aeeecdbbfc79557273b38fe53f
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-09-22 21:12:36 +0500

    feat: Commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'revert',
+        'ci',
+        'perf',
+        'build',
+        'release',
+        'deps',
+        'config',
+        'security',
+      ],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
